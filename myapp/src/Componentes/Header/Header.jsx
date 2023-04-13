cc23aec520cf55708323740db7a3ae0ee0f7bc94
Marcosfarina2022 <marcosfarina@tdf.edu.ar>
2023-04-12 23:19:15 -0300

    creación de un Header, utilizando react con estilos

---
+++ myapp/src/Componentes/Header/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+const Header = ()=>{// una constante apunta a una dirección de memoria y guarda un lugar ahi va a guardar un objeto, si nosotros queremos que el objeto sea juan o lisandro se puede hacer, lo que no se puede hacer es que el objeto pase a ser un string.  
+    return (
+        <div className='Header'>
+        <h3>Header</h3>
+        </div>
+    )
+}
+export default Header;
